f0fb9a637b1df84b5f54cc8dae17d0eefde475dd
ckcodes907 <137890733+ckcodes907@users.noreply.github.com>
2023-12-17 10:16:09 +0530

    this_is_test

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'back-img': "url('/Users/chhavikantmahobia/Downloads/rp/dist/Rectangle_121.png')"
+       
+      }
+  },
+  plugins: [],
+}
+}
+
